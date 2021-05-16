086a43a724bf08e6d0a3dc1d8446d89c27d44487
wemmelotec <wellington.magno@gmail.com>
2021-05-16 06:16:01 -0300

    primeira execução da api

---
+++ src/main/java/br/com/zup/api/ZupApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/api/ZupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
